ab65bd9ccf11fab284338fc2d14164fe8a584bdd
Akshat2480 <akshatworkstuffs@gmail.com>
2026-08-31 17:46:58 +0530

    solve: if else

---
+++ 01_basics/A_Things_to_know/if_else.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    void studentGrade(int marks) {
+        if(marks >= 90){
+            cout<<"Grade A";
+        }
+        else if(marks >= 70){
+            cout<<"Grade B";
+        }
+        else if(marks >= 50){
+            cout<<"Grade C";
+        }
+        else if(marks >= 35){
+            cout<<"Grade D";
+        }
+        else{
+            cout<<"Fail";
+        }
+    }
+};
+
+int main(){
+    Solution s;
+    s.studentGrade(5);
+    return 0;
+}
