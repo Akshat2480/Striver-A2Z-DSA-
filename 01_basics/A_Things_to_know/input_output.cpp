5d4f09209d43b9f69cb6bca7044a6b1d4214518c
Akshat2480 <akshatworkstuffs@gmail.com>
2026-08-31 17:05:13 +0530

    solve: input output

---
+++ 01_basics/A_Things_to_know/input_output.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    void printNumber() {
+        int x;
+        cin >> x;
+        cout << x;
+    }
+};
+
+int main() {
+    Solution s;
+    s.printNumber();
+    return 0;
+}
