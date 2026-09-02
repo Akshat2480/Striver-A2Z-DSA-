457aba568352979aec2594590c83006a7ea72001
Akshat2480 <akshatworkstuffs@gmail.com>
2026-09-02 18:26:33 +0530

    solve: pattern 12

---
+++ 01_basics/B_Patterns/pattern_12.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    void pattern12(int n) {
+        for(int i = 1; i<=n; i++){
+            for(int j=1; j<=i; j++){
+                cout<<j;
+            }
+
+            for(int j=1; j<=2*(n-i); j++){
+                cout<<" ";
+            }
+
+            for(int j=i; j>=1; j--){
+                cout<<j;
+            }
+            cout<<endl;
+        }
+    }
+};
+
+int main(){
+    Solution s;
+    s.pattern12(5);
+    return 0;
+}
