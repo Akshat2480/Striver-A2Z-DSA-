43b4bf189322a55cc89b753356a55041a87d7ec9
Akshat2480 <akshatworkstuffs@gmail.com>
2026-09-11 11:18:36 +0530

    learned: recursion print 1 to n

--- 01_basics/E_Basic_Recursion/print_1_to_n.cpp
+++ 01_basics/E_Basic_Recursion/print_1_to_n.cpp
@@ -1,15 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void print(int i, int n){
-    if(i>n) return; 
-    cout<<i<<endl;
-    print(i+1, n);
+void print(int n){
+    if(n<1) return; 
+    print(n-1);
+    cout<<n<<endl;
 }
 
 int main() {
     int n;
     cin>>n;
-    print(1, n);
+    print(n);
     return 0;
 }
