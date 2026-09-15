4db4d8dc8f8fd4ac10e689c593874f2cbb7eda6b
Akshat2480 <akshatworkstuffs@gmail.com>
2026-09-15 12:08:13 +0530

    learned: merge sort

---
+++ 02_Sorting_Techniques/merge_sort.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    void merge(vector<int>& nums, int low, int mid, int high){
+        vector<int> temp;
+        int left = low, right = mid+1;
+        while(left<=mid && right<=high){
+            if(nums[left] <= nums[right]){
+                temp.push_back(nums[left]);
+                left++;
+            }
+            else{
+                temp.push_back(nums[right]);
+                right++;
+            }
+        }
+        while(left<=mid){
+            temp.push_back(nums[left]);
+            left++;
+        }
+        while(right<=high){
+            temp.push_back(nums[right]);
+            right++;
+        }
+    
+        for(int i=low; i<=high; i++){
+            nums[i] = temp[i - low];
+        }
+    }
+
+    void merge_sort(vector<int>& nums, int low, int high){
+        if(low >= high) return;
+        int mid = (low + high) / 2;
+        merge_sort(nums, low, mid);
+        merge_sort(nums, mid+1, high);
+        merge(nums, low, mid, high);
+    }
+
+    vector<int> mergeSort(vector<int>& nums) {
+        merge_sort(nums, 0, nums.size()-1);
+        return nums;
+    }
+};
+
+
+int main() {
+    vector<int> nums = {7, 4, 1, 5, 3};
+    
+    Solution s;
+    vector<int> res = s.mergeSort(nums);
+
+    for(auto it : res){
+        cout<<it<<" ";
+    }
+    
+    return 0;
+}
